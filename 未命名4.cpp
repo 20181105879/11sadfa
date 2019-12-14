f60e221ebd1b97672e72acc5f8d7dac00d54f86f
20181105879 <2605753145@qq.com>
2019-12-14 21:56:28 +0800

    Initial commit

---
+++ 未命名4.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+class CMyClassA {
+    int val;
+public:
+    CMyClassA(int);
+    void virtual print();
+};
+CMyClassA::CMyClassA(int arg) {
+    val = arg;
+    printf("A:%d\n", val);
+}
+void CMyClassA::print() {
+    printf("%d\n", val);
+    cout<<"1111";
+    return;
+}
+
+/* 在这里填写代码 */
+class CMyClassB :public CMyClassA{
+	public :
+	CMyClassB(int t=0):CMyClassA(3*t)
+	{
+		val=t;
+		
+		printf("B:%d\n", val);	
+	}
+	void virtual print()
+	{
+	 printf("%d\n", val);
+	 cout<<"22222";
+	}
+	private:
+		int val;
+
+}; 
+
+
+int main(int argc, char** argv) {
+    CMyClassA a(3), *ptr;
+    CMyClassB b(5);
+    ptr = &a;
+    ptr->print();
+    a = b;
+    a.print();
+    ptr = &b;
+    ptr->print();
+    cout<<endl<<endl;;
+    a.print();
+    return 0;
+}
